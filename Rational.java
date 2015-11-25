3cab189710f92a3f087c33447bdcbd1d17664c6c
Yanni <Yanni@cinder.local>
2015-11-24 22:34:19 -0500

    checked, updated, organized

--- Rational.java
+++ Rational.java
@@ -1,16 +1,18 @@
-/*
-The Don't Café - Matthew So & Jannie Li
-APCS1 pd10
-HW37 -- Rational Equality
-2015-11-25
-*/
+// The Don't Cafe - Matthew So & Jannie Li
+// APCS1 pd10
+// HW37 -- Rational Equality
+// 2015-11-25
+
 
 public class Rational{
-  /*=====Instance=Variables=====*/
-  private int numerator;
-  private int denominator;
+    
+    /*=====Instance=Variables=====*/
+    private int numerator;
+    private int denominator;
 
-  /*=====Accessors=====*/
+
+    
+    /*=====Accessors=====*/
     public int getNumerator() {
 	return numerator;
     }
@@ -18,16 +20,20 @@ public class Rational{
     public int getDenominator() {
 	return denominator;
     }
+
+
     
-  /*=====Constructors=====*/
-  public Rational() {
-    //Creates a new Rational with the value of 0/1
+    /*=====Constructors=====*/
+    public Rational() {
+	//Creates a new Rational with the value of 0/1
 	this(0,1);
     }//end basic constructor
-
+    
+    
     public Rational(int num, int denom) {
-    //takes 2 parameters, one for the numerator, one for the denominator
-    //if an invalid denominator is attempted, should print a message and set the number to 0/1
+	//takes 2 parameters, one for the numerator, one for the denominator
+	//if an invalid denominator is attempted,
+	//should print a message and set the number to 0/1
 	denominator = denom;
 	numerator = num;
 	if (denom == 0) {
@@ -36,47 +42,63 @@ public class Rational{
 	    denominator = 1;
 	}
     }//end constructor
-  
-  /*=====Methods=====*/
-  public String toString(){
-    //returns a string representation of the rational number (formatting of your choice)
-    //example for us "3/2" -EM
-      String ret = numerator + "/" + denominator;
-      return ret;
-  }
-  
-  public double floatValue(){
-    //Returns a floating point value of the number
-    //Uses the most precise floating point primitive (which is double btw i think-EM)
+
+
+
+    
+    /*=====Methods=====*/
+    public String toString(){
+	//returns a string representation of the
+	//rational number (formatting of your choice)
+	//example for us "3/2" -EM
+	String ret = numerator + "/" + denominator;
+	return ret;
+    }
+
+
+    public double floatValue(){
+	//Returns a floating point value of the number
+	//Uses the most precise floating point primitive
+	//(which is double btw i think-EM)
     	double numer = numerator;
     	double denom = denominator;
-      double ret = numer/denom;
-      return ret;
-  }
-  
-  public void multiply(Rational multiplier){
-    //Takes 1 Rational object as a parameter and multiplies it by this Rational object
-    //Does not return any value
-    //Should modify this object and leave the parameter alone (see below for example)
-    //Need not reduce the fraction
-      int num = numerator * multiplier.getNumerator();
-      int denom = denominator * multiplier.getDenominator();
-      numerator = num;
-      denominator = denom;
-  }
+	double ret = numer/denom;
+	return ret;
+    }
+
     
-  public void divide(Rational divisor){
-    //Works the same as multiply, except the operation is division
-    Rational inverse = new Rational(divisor.getDenominator(), divisor.getNumerator());
-    multiply(inverse); //multiplies by the inverse of the divisor
-  }
+    public void multiply(Rational multiplier){
+	//Takes 1 Rational object as a parameter and multiplies
+	//it by this Rational object
+	//Does not return any value
+	//Should modify this object and leave the parameter
+	//alone (see below for example)
+	//Need not reduce the fraction
+	int num = numerator * multiplier.getNumerator();
+	int denom = denominator * multiplier.getDenominator();
+	numerator = num;
+	denominator = denom;
+    }
+
+ 
+    public void divide(Rational divisor){
+	//Works the same as multiply, except the operation is division
+	Rational inverse = new Rational(divisor.getDenominator(),
+					divisor.getNumerator());
+	multiply(inverse); //multiplies by the inverse of the divisor
+    }
+
+    
     public void add(Rational x){
 	numerator = (numerator * x.denominator) + (x.numerator * denominator);
 	denominator = (denominator * x.denominator);}
+
+
     public void subtract(Rational x){
 	numerator = (numerator * x.denominator) - (x.numerator * denominator);
 	denominator = (denominator * x.denominator);}
-    
+
+
     //returns gcd, needs int a to be greater than int b!!!
     public static int gcd(int a, int b) {
 
@@ -84,7 +106,8 @@ public class Rational{
 	int larger = Math.max(a, b);
 	int smaller = Math.min(a, b);
 	
-	//for the case that the larger number is divisible by the smaller number
+	//for the case that the larger number is divisible
+	//by the smaller number
 	if ( larger % smaller == 0 ) {
 	    return smaller;
 	}
@@ -104,25 +127,34 @@ public class Rational{
 	}
     }// end of gcdHelper() method
 
+
+    
     //gcd with different signature than above
-    //takes no inputs and returns gcd of the current numerator and denominator of the instance of class
+    //takes no inputs and returns gcd of the current
+    //numerator and denominator of the instance of class
     public int gcd(){
 	return gcd(numerator,denominator);}
 
-    // reduce
-    // Changes this Rational to one in reduced form (should use gcd)
+    
+
+    //reduce
+    //Changes this Rational to one in reduced form (should use gcd)
     //Uses gcd to bring the rational number into simplest form
     public void reduce(){
 	int gcd = gcd();
 	denominator = denominator/gcd;//just divide both numerator
 	numerator = numerator/gcd;
     }//end denominator by gcd
+
+    
     
     //Compares the values of two Rational numbers
-    //  Takes a Rational as a parameter and compares it to the calling object
-    // Returns 0 if the two numbers are equal
-    // Returns a positive integer if the calling number is larger than the parameter
-    // Returns a negative integer if the calling number is smaller than the parameter
+    //Takes a Rational as a parameter and compares it to the calling object
+    //Returns 0 if the two numbers are equal
+    //Returns a positive integer if the calling number is
+    //larger than the parameter
+    // Returns a negative integer if the calling number is
+    //smaller than the parameter
     public int compareTo(Rational other){
 	if (this.floatValue() > other.floatValue()){
 	    return 1;}
@@ -132,26 +164,37 @@ public class Rational{
 	    return -1;}
     }
 
+
+    
     // equals
     // Takes 1 Object as input
-    // Returns true if input is of class Rational and of equal value to calling instance of Rational
+    // Returns true if input is of class Rational and of
+    //equal value to calling instance of Rational
     // Returns false otherwise
     public boolean equals(Object other){
-	if (other.getClass() == this.getClass()){ //compares the instance variables in the two objects
-	    Rational newother = (Rational)other; //create a rational instance of the inout
+	if (other.getClass() == this.getClass()){
+	    //compares the instance variables in the two objects
+	    
+	    Rational newother = (Rational)other;
+	    //create a rational instance of the input
+	    
 	    newother.reduce(); //reduce both
 	    this.reduce();
-	    if (this.numerator == newother.numerator && this.denominator == newother.denominator){ //compare
-		return true;}} //return true if the same
-	return false;} //false in all other situations
+	    if (this.numerator == newother.numerator &&
+		this.denominator == newother.denominator)
+		{ return true;}} //return true if the same
 	
-  /*=====Example=of=Multiply=====
-  Rational r = new Rational(2,3); //Stores the rational number 2/3
-  Rational s = new Rational(1,2); //Stores the rational number 1/2
-  r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains 1/2*/
+	return false;} //false in all other situations
+
+    
+	
+    /*=====Example=of=Multiply=====
+      Rational r = new Rational(2,3); //Stores the rational number 2/3
+      Rational s = new Rational(1,2); //Stores the rational number 1/2
+      r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains 1/2*/
   
-  /*=====Main=Method=====*/
-  public static void main (String[] args){
+    /*=====Main=Method=====*/
+    public static void main (String[] args){
   	Rational test1 =  new Rational();//tests default constructor;
   	
   	System.out.println(test1);//tests toString
@@ -201,6 +244,7 @@ public class Rational{
 	System.out.println(test8.compareTo(test10)); //should be -1
 
 	
+	//HW37 TESTING
   	//testing equals()
 	Rational test11 = new Rational(1,2);
 	Rational test12 = new Rational(5,10);
@@ -208,5 +252,5 @@ public class Rational{
 	System.out.println(test11.equals(test12)); //return true
 	System.out.println(test11.equals(test13)); //return false
 	System.out.println(test13.equals(test12)); //return false
-  }
+    }
 }
